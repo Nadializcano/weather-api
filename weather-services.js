c80f9c4dce3149a368e588af905a22274c1120a4
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-06-04 16:28:10 -0700

    initial

---
+++ weather-services.js
@@ -0,0 +1,17 @@
+class WeatherService {
+  getWeatherByCity(city) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=01041cd9da2578365839304b00788727`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
